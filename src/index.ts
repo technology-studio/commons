0b49da0613e6f2e22fec93d43e53e1c45391001b
Erik Slovak <erik.slovak@technologystudio.sk>
2023-11-16 16:34:56 +0100

    chore: migrate package

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik>
+ * @Date:   2018-01-26T15:36:57+01:00
+ * @Email:  rostislav.simonik@technologystudio.sk
+ * @Copyright: Technology Studio
+**/
+
+import { ExtendableException } from './Api/ExtendableException'
+
+export {
+  ExtendableException,
+}
