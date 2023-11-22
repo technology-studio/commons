3445876c6a4f68eb33b5c1e48c903961f5a4fb95
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2023-11-22 08:55:02 +0100

    chore: migrate package (#20)
    * chore: migrate package
    
    * chore: use public workflow

---
+++ src/Api/ExtendableException.ts
@@ -0,0 +1,18 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik>
+ * @Date:   2017-05-03T19:10:18+02:00
+ * @Email:  rostislav.simonik@technologystudio.sk
+ * @Copyright: Technology Studio
+**/
+
+export class ExtendableException extends Error {
+  constructor (message: string) {
+    super(message)
+    this.name = this.constructor.name
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, this.constructor)
+    } else {
+      this.stack = (new Error(message)).stack
+    }
+  }
+}
